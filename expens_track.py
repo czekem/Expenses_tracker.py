a82d1ee11c3c2ead8f5bef32620d4df6bdc69439
Michael_Cz <145675402+czekem@users.noreply.github.com>
2024-01-10 09:55:40 +0100

    Update expens_track.py

--- expens_track.py
+++ expens_track.py
@@ -1,45 +1,63 @@
-import argparse
-from typing import List, Dict
-import click
-import pandas as pd 
-import pickle
 import os
 import csv
-import xlrd 
-import sqlite3
+import pickle
+import pandas as pd
+from dataclasses import dataclass
+from typing import List, Dict
+import click
 import inquirer
-from collections import Counter
 
+@dataclass
 class Cost:
-    """This class allow to create cost object, and use tag to check if it exists"""
-    next_id = 1
-    def __init__(self, description: str, cost: float, time: str, tag: str, id=None):
-        self.id = Cost.next_id
-        self.description = description
-        self.cost = cost
-        self.time = time
-        self.tag = tag
-        Cost.next_id += 1
+    """
+    This class allow to create cost object, and use tag to check if it exists,
+    and use 'big' to check if it is big cost
+    """
+    description: str
+    cost: float
+    time: str
+    tag: str
+    id: int = None
+    big: str = None
+
+    @property
+    def next_id(cls):
+        cls._next_id = getattr(cls, '_next_id', 1)
+        cls._next_id += 1
+        return cls._next_id
+
+    def __post_init__(self):
+        if self.id is None:
+            self.id = self.next_id
+        if self.cost >= 1000:
+            self.big = '!!!'
+
     def __str__(self):
-        return f'id: {self.id}, description: {self.description}, cost: {self.cost}, time: {self.time}, tag: {self.tag}'
-   
+        return f'id: {self.id}, description: {self.description}, cost: {self.cost}, time: {self.time}, tag: {self.tag}, big: {self.big}'
+
     def __repr__(self):
         return f'id: {self.id!r}, description: {self.description!r}, cost: {self.cost!r}, time: {self.time!r}, tag: {self.tag!r}'
 
-   
+    def __eq__(self, other):
+        return (self.id == other.id, self.description == other.description, self.cost == other.cost, self.time == other.time, self.tag == other.tag, self.big == other.big)
+
+
 def cost_add() -> List[Cost]:
-    """This function allow to add cost to the list, and return it as the cost_list"""
+    """
+    This function allow to add cost to the list, and return it as the cost_list
+    """
     costs_list = []
     while True:
         questions = [
             inquirer.Text('description', message="Please enter the description"),
             inquirer.Text('cost', message="Please enter the cost"),
-            inquirer.Text('time', message="Please enter the time"),
+            inquirer.Text('time', message="Please enter the date"),
             inquirer.Text('tag', message="Please enter the tag")
         ]
         answers = inquirer.prompt(questions)
+        
         try:
-            costs_list.append(Cost(answers['description'], answers['cost'], answers['time'], answers['tag']))
+            costs_list.append(Cost(answers['description'], float(answers['cost']), answers['time'], answers['tag']))
         except ValueError:
             print("Invalid input, please enter id, cost and time as numbers.")
         next_cost_value = input('Do you want to add another cost? (y/n): ').lower()
@@ -49,20 +67,24 @@ def cost_add() -> List[Cost]:
 
 
 def format_file():
-    """This function allow to check if the user want to save the file in csv, 
-    xlsx or db format"""
+    """
+    This function allow to check if the user want to save the file in csv, 
+    xlsx or db format
+    """
     questions = [
        inquirer.List('format',
-                    message="How do you want to save your file?",
-                    choices=['csv', 'xlsx', 'db'],
-                    ),
-   ]
+                     message="How do you want to save your file?",
+                     choices=['csv', 'xlsx', 'db'],
+                     ),
+]
     answers = inquirer.prompt(questions)
     return answers['format']
 
 
 def create_filename():
-    """This function allow user to create filename under the name that they want"""
+    """
+    This function allow user to create filename under the name that they want
+    """
     filename = input('Enter file name: ')
     if os.path.isfile(filename):
         overwrite = [inquirer.List('format', message="File already exists. Do you want to overwrite it?", choices=['y', 'n'],)]
@@ -78,11 +100,39 @@ def create_filename():
             return filename
 
 
+def save_db(cost_adding_list, format, filename):
+    """
+    This function allow to save a db file
+    """
+    if format == 'db':
+        if os.path.isfile(filename):
+            overwrite = input('File already exist. Do you want to overwrite it? [y/n]: ').strip().lower()
+            if overwrite == 'n':
+                question = input('Do you want to add data to the existing file? [y/n]: ').strip().lower()
+                if question == 'y':
+                    full_filename = filename + '.' + format  # lub os.join(filename, format)
+                    with open(full_filename, '+xb') as stream:
+                        pickle.dump(cost_adding_list, stream)
+                        print('The data for {full_filename} has been added.')
+                else:
+                    print('File not save due to the existing file.')
+                    
+        else:
+            full_filename = filename + '.' + format 
+            with open(full_filename, 'wb') as stream:
+                pickle.dump(cost_adding_list, stream)
+            print(f'File {full_filename} has been saved in database.')
+
+
 def save_file(filename: str, columns: List[str], values: List[List[str]], format: str) -> None: 
-    """This function is saving named file, first checking if there is no file with such name,
-    if it is not there, it will create new file and save it in the right format.
-    If there is a file with such name, it will ask user if he wants to overwrite it,
-    or add data to it."""
+    """This function is saving named file, 
+    first checking if there is no file with such name,
+    if it is not there, 
+    it will create new file and save it in the right format.
+    If there is a file with such name, it will ask user if he wants to
+    overwrite it,
+    or add data to it.
+    """
     df = pd.DataFrame({col: values[i] for i, col in enumerate(columns)})
     full_filename = filename + '.' + format 
     if os.path.isfile(full_filename):
@@ -94,26 +144,23 @@ def save_file(filename: str, columns: List[str], values: List[List[str]], format
                 return
             elif question == 'y':
                 if format == 'csv':
-                   df.to_csv(full_filename, mode='a', index=False, header=False)
+                    df.to_csv(full_filename, mode='a', index=False, header=False)
                 elif format == 'xlsx':
-                   with pd.ExcelWriter(full_filename, engine='openpyxl', mode='a') as writer:
-                       df.to_excel(writer, index=False, header=False)
-                elif format == 'db':
-                   # Handle database file here
-                   pass
+                    existing_df = pd.read_excel(full_filename)
+                    combined_df = pd.concat([existing_df, df], ignore_index=True)
+                    combined_df.to_excel(full_filename, index=False)
     else:
         if format == 'csv':
             df.to_csv(full_filename, index=False)
         elif format == 'xlsx':
             df.to_excel(full_filename, index=False)
-        elif format == 'db':
-            # Handle database file here
-            pass
     print('File saved successfully.')
 
 
 def file_open():
-    """This function is opening the file that user will name to work with it"""
+    """
+    This function is opening the file that user will name to work with it
+    """
     filename = input("Please write the name of the file and it's extension:\n").lower()
     file, extension = filename.rsplit('.', maxsplit=1)
     if extension == 'csv':
@@ -131,17 +178,18 @@ def file_open():
         return entries
 
 def print_in_sorted_way(opening_file: List[Cost]):
-    print('=ID= ==DESC== ==COST== ==DATE== ==TAG==')
+    """
+    This code is printing file data and values in sorted way
+    """
+    print('=ID= ==DESC== ==COST== ==DATE== ==TAG== ==BIG==')
     for cost in opening_file:
-        if cost.cost >= 1000:
-          print(f'{cost.id} {cost.description} {cost.cost}, {'''!!!''' if cost.cost >= 1000 else ''} {cost.time} {cost.tag}')  
-        else:
-            print(f'{cost.id} {cost.description} {cost.cost} {cost.time} {cost.tag}')
-    df = pd.DataFrame({vars:(cost)for cost in opening_file})
-    df.columns = ['ID', 'DESC','COST', 'DATE', 'TAG']
+        print(f'{cost.id} {cost.description} {cost.cost}, { cost.cost} {cost.time} {cost.tag } {cost.tag} {cost.big} ')  # te wyrażenie zwracało tuple tak lepiej  # # print(f'{cost.id} {cost.description} {cost.cost, '!!!'} {cost.time} {cost.tag}')
 
+    
 def tags_check(entries: List[Cost], tag_input: str) -> bool:
-    """This function is checking for specyfic tag in file to group them"""
+    """
+    This function is checking for specyfic tag in file to group them
+    """
     for entry in entries:
         if tag_input in entry.tag:
             return True
@@ -149,54 +197,66 @@ def tags_check(entries: List[Cost], tag_input: str) -> bool:
 
 
 def whole_tags(entries: List[Cost],tag_input: str) -> Dict[str, float]:
-    """This function is allow to show the user 
-    all costs that have specific tag"""
+    """
+    This function is allow to show the user 
+    all costs that have specific tag
+    """
     costs_by_tag = {}
     for entry in entries:
         if entry.tag in costs_by_tag:
             costs_by_tag[entry.tag] += entry.cost
         else:
             costs_by_tag[entry.tag] = entry.cost
-    return f' The total cost of {tag_input} is: {costs_by_tag[tag_input]}' 
+    return f' The total cost of {tag_input} is: {costs_by_tag[tag_input]}'  # costs_by_tag[tag_input] # if We delete {tag_input} it will show all costs and tags names
+
 
 def total_costs(entries: List[Cost]) -> float:
     total = 0
     for entry in entries:
         total += entry.cost
-    return f'Total costs in column "cost" is: {total}'
+    print(f"Total costs in column 'cost' is: ", round(total, 2))
 
 
+@click.group()
+def cli():
+    pass
 
-@click.command
-def main():
-    """This script helps you to manage your costs.
-    It's divided to 2 sections. First is to add costs 
-    and second is to read them, also by tags"""
-    choose_action = input('Do you want to write or read cost? (write/read): ').lower()
-    if choose_action == 'write':
-        cost_adding_list = cost_add()
+
+@cli.command()
+def write():
+    """
+    This function allow user to add data to file,
+    it core function to add the cost data and save it to file
+    """
+    
+    cost_adding_list = cost_add()
         
-        id = [entry.id for entry in cost_adding_list]
-        description = [entry.description for entry in cost_adding_list]
-        cost = [entry.cost for entry in cost_adding_list]
-        time = [entry.time for entry in cost_adding_list]
-        tag = [entry.tag for entry in cost_adding_list]
+    id = [entry.id for entry in cost_adding_list]
+    description = [entry.description for entry in cost_adding_list]
+    cost = [entry.cost for entry in cost_adding_list]
+    time = [entry.time for entry in cost_adding_list]
+    tag = [entry.tag for entry in cost_adding_list]
         
-        format = format_file()
-        filename = create_filename()
+    format = format_file()
+    filename = create_filename()
+    if format == 'db':
+        save_file_db = save_db(cost_adding_list, format, filename)
+    else:
         saving_the_cost_list = save_file(filename, ['id', 'description', 'cost', 'time', 'tag'], [id, description, cost, time, tag], format=format)
     
-    elif choose_action == 'read':
+
+@cli.command()
+def read():
+    if read:
         opening_file = file_open()
         print_in_adjecent_columns = print_in_sorted_way(opening_file)
         tag_input = input('Please enter tag that you want to check:\n').lower()
         checking_tags = tags_check(opening_file, tag_input)
         print(f'Tag "{tag_input}" exists: {checking_tags}')
-        print_data_with_tags = whole_tags(opening_file,tag_input)
+        print_data_with_tags = whole_tags(opening_file, tag_input)
         print(print_data_with_tags)
         total_cost = total_costs(opening_file)
         print(total_cost)
 
-
 if __name__ == "__main__":
-    main()
+    cli()
